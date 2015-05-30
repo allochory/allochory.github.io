df9c62ab84581a2df6a8e27a56922debd871a16a
Tyler Abbot <tyler.abbot@sciencespo.fr>
2015-05-30 13:53:45 +0200

    2015.05.30.1

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,6 +21,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\r\n\r\n"."Here are the details:\r\n\r\nName: $name\r\n\r\nEmail: $email_address\r\n\r\nPhone: $phone\r\n\r\nMessage:\r\n$message";
 $headers = "From: contact@allochory.org\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers, "-fcontact@allochory.org");
+mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
